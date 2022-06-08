800562498771b13b734bfc0f1522ed51650c4a11
avc-tech <avctech.official@gmail.com>
2022-06-08 20:12:44 +0530

    done

--- home/models.py
+++ home/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Task(models.Model):
+	order = models.IntegerField(default=None,unique=True,blank=False,null=False)
+	code = models.TextField(default=None,unique=True,blank=False,null=False)
+	hint_html = models.TextField(default=None,unique=True,blank=False,null=False)
+	
--- home/views.py
+++ home/views.py
@@ -1,9 +1,40 @@
 from django.shortcuts import render
-
+from .models import *
 # Create your views here.
 
 def index(request):
 	db = {
-		
+		"post":False,
+		"valid":False,
+		"message":{
+			"text":"",
+		}
 	}
-	return render(request,"index.html",{})
+	if request.method == "POST":
+		post = request.POST
+		code = post["code"]
+		objs = Task.objects.filter(code=code)
+		if len(objs) < 1:
+			db["post"] = True
+			db["valid"] = False
+			db["message"]["text"] = """
+			<span class="info">Wrong code. Search more... and try again. 🤗</span>
+			<br/><a href=""><button>Try again</button></a>
+			"""
+			
+			return render(request,"index.html",db)
+		else:
+			try:
+				obj = objs[0]
+				db["post"] = True
+				db["valid"] = True
+				db["message"]["text"] = obj.hint_html
+			except:
+				db["post"] = True
+				db["valid"] = False
+				db["message"]["text"] = """
+				<span class="info">Wrong code. Search more... and try again. 🤗</span>
+				<br/><a href=""><button>Try again</button></a>
+				"""
+			return render(request,"index.html",db)
+	return render(request,"index.html",db)
