fd0cf7fd397b08bad73d95482c04aaeadccac387
avc-tech <avctech.official@gmail.com>
2022-06-10 12:23:53 +0530

    h

--- hunt/settings.py
+++ hunt/settings.py
@@ -13,8 +13,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = "CHANGE_ME!!!! (P.S. the SECRET_KEY environment variable will be used, if set, instead)."
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-SECURE_SSL_REDIRECT = False
+DEBUG = False
+SECURE_SSL_REDIRECT = True
 
 ALLOWED_HOSTS = ["*"]
 
